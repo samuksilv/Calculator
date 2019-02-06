8c9db9d8208426c93ba73a75700680d320a5b0d8
samuksilv <samuel.silv.carvalho@gmail.com>
2019-02-06 01:19:02 -0200

    criação do controller

---
+++ Calculator.Web/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using Calculator.Business.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Calculator.Web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalcularTroco(PagamentoContaViewModel pagamentoContaViewModel)
+        {
+            try
+            {
+                var calculator = new CalculaTrocoViewModel(pagamentoContaViewModel);
+
+                return Json(calculator.CalcularTroco(), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                string error = $"Erro valores inválidos. {ex.Message}";
+                return Json(error, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
